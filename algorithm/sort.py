061c2cbda7e6644f8074e9917772a4ff97da3c42
kingssafy <onlyforfile@gmail.com>
2019-01-16 18:00:29 +0900

    heap sort

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -9,8 +9,8 @@ a = [3, 2, 1, 5, 6, 4, 3, 8, 10, 9]
 
 def left(sequence, i):
     """Returns you left child"""
-    if len(sequence) >= 2 * i:
-        return sequence[2*i]
+    if len(sequence) >= 2 * (i + 1): # we need (index+1)*2 length of sequence
+        return sequence[2*i + 1] #abbriviation of 2(i+1)-1
     return None
 def right(sequence, i):
     if len(sequence) >= 2 * i + 1:
