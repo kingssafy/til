a09a76609941551d379763c5c274cfa14cbec8ba
kingssafy <onlyforfile@gmail.com>
2019-01-16 17:44:42 +0900

    heapsort

---
+++ algorithm/sort.py
@@ -0,0 +1,19 @@
+a = [3, 2, 1, 5, 6, 4, 3, 8, 10, 9]
+#BUILD-MAX-HEAP(a)
+"""
+                3
+        2               1
+    5        6      4        3
+  8  10    9
+"""
+
+def left(sequence, i):
+    """Returns you left child"""
+    if len(sequence) >= 2 * i:
+        return sequence[2*i]
+    return None
+def right(sequence, i):
+    if len(sequence) >= 2 * i + 1:
+        return sequence[2*i + 1]
+
+print(left(a, 1))
