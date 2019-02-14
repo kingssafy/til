03ce12612eec5125989ce4b1a97296957b1a572c
kingssafy <onlyforfile@gmail.com>
2019-02-14 13:40:13 +0900

    learnning js

--- ps/ladder1.py
+++ ps/ladder1.py
@@ -1,19 +1,11 @@
 import sys
-sys.stdin = open("input.txt")
+sys.stdin = open("ladder1_input.txt")
 
 for _ in range(10):
-    int(input())
-    ladder = [list(map(int, input().split())) for _ in range(100)]
-    x, y = 99, ladder[99].index(2) 
-    while x > 0:
-        if y+1 < 100 and ladder[x][y+1]:
-            ladder[x][y] = 0
-            y += 1
-            continue
-        if y-1 > -1 and ladder[x][y-1]:
-            ladder[x][y] = 0
-            y -= 1
-            continue
-        else:
-            x -= 1
+    input()
+    ladder = ["".join(input().split()) for _ in range(100)]
+    y = ladder[99].find("2")
+    for row in reversed(ladder):
+        if y+1 < 100 and int(row[y+1]): y = row.rfind("1"); continue
+        if y-1 > -1 and int(row[y-1]): y= row.find("1"); continue
     print(f"#{_+1} {y}")
